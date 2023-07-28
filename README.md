7a8062bad0179d21666dbb9dc40ea3087aaaee19
Jason Miller <info@nooiq.co>
2023-07-27 21:30:12 -0400

    added external TLD list for validation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Google-EmailScraper-Reloaded is a tool that searches Google based on a query and scrapes all emails found on each page. The output files are saved as csv. Based on Google-EmailScraper for Python 2.x and xgoogle by Kenny Ledet (https://github.com/kennyledet/Google-EmailScraper).
 
+This repo contains a list of valid TLD's (tld.txt) which aids in email validation. You may edit it to fit your needs.
+
 ## Installation
 
 1. Clone the repository.
@@ -19,10 +21,16 @@ python main_serp.py -query "Your Search Query" -pages 10 -o "output.csv" -key "Y
 
 Where:
 
-- `-query` is your search query.
-- `-pages` is the number of Google results pages to scrape (default is 10).
-- `-o` is the output filename (default is 'emails.csv').
-- `-key` is your Serp API key.
+  - `-query` is your search query.
+  - `-pages` is the number of Google results pages to scrape (default is 10).
+  - `-o` is the output filename (default is 'emails.csv').
+  - `-key` is your Serp API key.
+
+There are three optional flags for post-processing:
+
+  -P: Enable post-processing. This will convert all emails to lowercase, remove duplicates, and validate the emails.
+  -Eo: Email only output. This will only include emails in the output CSV, not the source page info.
+  -Ng: Exclude .gov emails. This will remove all .gov emails, as well as emails from sheriff, county, and federal domains.
 
 You can provide the Serp API key either directly as a command line argument using `-key`, or you can set it in a `.env` file with the variable name `SERP_API_KEY`. The script will automatically load the API key from the `.env` file if it's not provided in the command line.
 
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-# Google-EmailScraper
-# 
-# Purpose: Scraper that searches Google based on a query and scrapes all emails found on each page.
-# Output files are saved as csv.
-# 
-# Author: Jason
-# Date: 2023-07-27
-# 
-# This program is released under the MIT License.
-# Credit for the defunct Python 2 version: https://github.com/kennyledet/Google-EmailScraper
-# 
-# This version uses the Serp API instead of the deprecated xGoogle library.
-
 from serpapi import GoogleSearch
 from urllib.request import Request, urlopen
 import re, csv, os
 import argparse
 from dotenv import load_dotenv
+from datetime import datetime
 
 class ScrapeProcess(object):
-    emails = []  # for duplication prevention
+    emails = {}  # for duplication prevention and post-processing
 
-    def __init__(self, filename):
-        self.filename  = filename
-        self.csvfile   = open(filename, 'w+')
+    def __init__(self, filename, email_only, no_gov):
+        if os.path.isfile(filename):
+            filename = f"{os.path.splitext(filename)[0]}_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
+        self.filename = filename
+        self.email_only = email_only
+        self.no_gov = no_gov
+        self.csvfile = open(filename, 'w+')
         self.csvwriter = csv.writer(self.csvfile)
+        
+        # Load TLD list
+        with open('tld.txt', 'r') as tld_file:
+            self.tld_list = [line.strip() for line in tld_file]
+
+    def validate_email(self, email):
+        email_regex = re.compile(r'[^@]+@[^@]+\.[^@]+')
+        return email_regex.match(email)
+
+    def get_tld(self, email):
+        return email.split('.')[-1]
 
     def go(self, query, pages):
         load_dotenv()
@@ -41,7 +44,9 @@ class ScrapeProcess(object):
             print(f'Number of results: {len(results)}')
             for page in results:
                 self.scrape(page)
-            
+
+        self.post_process()
+
     def scrape(self, page):
         try:
             request = Request(page['link'])
@@ -54,19 +59,37 @@ class ScrapeProcess(object):
         emails = re.findall(r'([A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*)', html)
 
         for email in emails:
-            if email not in self.emails:  # if not a duplicate
+            email = email.lower()
+            if self.no_gov and re.search(r'\.gov|sheriff|county|federal', email):
+                continue
+            if email[-1] == '.':
+                tld = email.split('.')[-2]
+                if tld in ['com', 'net', 'org', 'edu', 'gov']:
+                    email = email[:-1]
+            if re.search(r'\d$', email):
+                continue
+            if email not in self.emails and self.validate_email(email) and self.get_tld(email) in self.tld_list:  # if not a duplicate and valid email with valid TLD
                 print(f'Found email: {email}')
-                self.csvwriter.writerow([page['title'], page['link'], email])
-                self.emails.append(email)
+                self.emails[email] = (page['title'], page['link'])
+
+    def post_process(self):
+        for email, (title, link) in self.emails.items():
+            if self.email_only:
+                self.csvwriter.writerow([email])
+            else:
+                self.csvwriter.writerow([title, link, email])
 
 parser = argparse.ArgumentParser(description='Scrape Google results for emails')
 parser.add_argument('-query', type=str, default='test', help='a query to use for the Google search')
 parser.add_argument('-pages', type=int, default=10, help='number of Google results pages to scrape')
 parser.add_argument('-o', type=str, default='emails.csv', help='output filename')
 parser.add_argument('-key', type=str, default=os.getenv('SERP_API_KEY'), help='Serp API key')
+parser.add_argument('-P', action='store_true', help='Enable post-processing (lowercase and dedupe)')
+parser.add_argument('-Eo', action='store_true', help='Email only output')
+parser.add_argument('-Ng', action='store_true', help='Exclude .gov emails')
 
-args   = parser.parse_args()
+args = parser.parse_args()
 args.o = args.o+'.csv' if '.csv' not in args.o else args.o  # make sure filename has .csv extension
 
-s = ScrapeProcess(args.o)
+s = ScrapeProcess(args.o, args.Eo, args.Ng)
 s.go(args.query, args.pages)
